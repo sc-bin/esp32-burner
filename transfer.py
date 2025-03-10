724177e45846c66fff4ca223aaa6c610c90a1117
sc-bin <3335447573@qq.com>
2025-03-10 13:39:33 +0800

    优化代码

--- transfer.py
+++ transfer.py
@@ -1,15 +1,46 @@
 import os
 import esptool
 
+banudrate = 1152000
+# banudrate = 921600
 
-class TRANSFER(object):
+
+class AMPY_OPS(object):
     tty_path: str
 
     def __init__(self, tty_path: str):
         self.tty_path = tty_path
+
+    def is_tty_exists(self) -> bool:
+        """判断tty路径是否存在"""
         if not os.path.exists(self.tty_path):
-            print(tty_path, "路径不存在")
-            exit(1)
+            print(self.tty_path, "路径不存在")
+            return False
+        else:
+            return True
+
+    def run(self, command: str) -> bool:
+        """运行命令,并返回是否运行成功"""
+        if not os.path.exists(self.tty_path):
+            print(self.tty_path, "路径不存在")
+            return False
+        ret = os.system(f"ampy --baud {banudrate} --port {self.tty_path} {command}")
+        if ret == 0:
+            print("运行成功")
+            return True
+        else:
+            print("运行失败")
+            return False
+
+    def run_with_return(self, command: str) -> str:
+        """运行命令，并返回该命令的输出"""
+        if not os.path.exists(self.tty_path):
+            print(self.tty_path, "路径不存在")
+            return False
+        ret = os.popen(
+            f"ampy --baud {banudrate} --port {self.tty_path} {command}"
+        )
+        return ret.read()
 
     def is_in_burn_mode(self) -> bool:
         """判断设备是否进入烧录模式"""
@@ -20,6 +51,39 @@ class TRANSFER(object):
         else:
             print("设备未进入烧录模式")
             return False
+
+    def send_file(self, py_path: str, dest_path="") -> bool:
+        """发送文件到micropython板子指定路径上"""
+        return self.run(f"put {py_path} {dest_path}")
+
+    def files_clear(self) -> bool:
+        """清除micropython板子上的文件"""
+        self.run(f"rmdir /")
+        return True
+
+    def mkdir_on_board(self, dir_name: str) -> bool:
+        """创建文件夹"""
+        # 判断mpy上是否已经有同名文件夹
+        if self.is_file_on_board(dir_name):
+            print("文件夹已存在")
+            return True
+        return self.run(f"mkdir {dir_name}")
+
+    def is_file_on_board(self, file_name: str) -> bool:
+        """判断文件是否存在"""
+        return self.run(f"ls | grep {file_name}")
+
+    def reset_mpy_board(self):
+        """重启micropython板子"""
+        return self.run(f"reset")
+
+    def run_py_file(self, file_name: str):
+        """运行电脑上的文件"""
+        return self.run(f"run {file_name}")
+
+
+class TRANSFER(AMPY_OPS):
+    tty_path: str
 
     def burner_picoW(self, firmware_path: str) -> bool:
         """烧录picoW固件"""
@@ -49,66 +113,5 @@ class TRANSFER(object):
             print("错误")
         return False
 
-    def send_file(self, py_path: str, dest_path="") -> bool:
-        """发送文件到micropython板子指定路径上"""
-        if not os.path.exists(py_path):
-            print(py_path, "不存在")
-            return False
-        print(self.tty_path, "发送文件", os.path.basename(py_path), "到", {dest_path})
-        ret = os.system(f"ampy --port {self.tty_path} put {py_path} {dest_path}")
-        if ret == 0:
-            print("发送成功")
-            return True
-        else:
-            print("发送失败")
-            return False
-
-    def files_clear(self) -> bool:
-        """清除micropython板子上的文件"""
-        ret = os.system(f"ampy --port {self.tty_path} rmdir /")
-        if ret == 0:
-            print("清除成功")
-            return True
-        else:
-            print("清除失败")
-            return False
-
-    def mkdir_on_board(self, dir_name: str) -> bool:
-        """创建文件夹"""
-        ret = os.system(f"ampy --port {self.tty_path} mkdir {dir_name}")
-        if ret == 0:
-            print("创建成功")
-            return True
-        else:
-            print("创建失败")
-            return False
-
-    def is_file_on_board(self, file_name: str) -> bool:
-        """判断文件是否存在"""
-        ret = os.system(f"ampy --port {self.tty_path} ls | grep {file_name}")
-        if ret == 0:
-            print(file_name, "文件存在")
-            return True
-        else:
-            print(file_name, "文件不存在")
-            return False
-
-    def reset_mpy_board(self):
-        """重启micropython板子"""
-        ret = os.system(f"ampy --port {self.tty_path} reset")
-        if ret == 0:
-            print("重启成功")
-            return True
-        else:
-            print("重启失败")
-            return False
-
-    def run_py_file(self, file_name: str):
-        """运行电脑上的文件"""
-        ret = os.system(f"ampy --port {self.tty_path} run {file_name}")
-        if ret == 0:
-            print("运行成功")
-            return True
-        else:
-            print("运行失败")
-            return False
+    def __init__(self, tty_path: str):
+        super().__init__(tty_path)
