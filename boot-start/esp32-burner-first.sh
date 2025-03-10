13ec3e9ddd1e75f8ed162ab54282fc42fa7fe7c7
sc-bin <3335447573@qq.com>
2025-03-10 14:52:43 +0800

    初次运行脚本 : 增加设置时区的命令

--- boot-start/esp32-burner-first.sh
+++ boot-start/esp32-burner-first.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 file_path=${BASH_SOURCE[0]}
-
+sudo timedatectl set-timezone Asia/Shanghai
 sudo systemctl enable lightdm.service
 sudo set-lcd lcd35-st7796 install
 sudo rm $shell_first
